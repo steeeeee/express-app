0c7290f1810fda2cfd8e27ac2d29c234325721fa
Stefano Giometti <stefano.giometti@gmail.com>
2016-06-22 02:07:27 +0200

    Listing cities on index.html

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+
 app.get('/', function(req, res) {
 	res.send('OK');
+});
+
+app.get('/cities', function(req, res) {
+	var cities = ['Lotopia','Caspiana','Indigo'];
+	res.json(cities);
 });
 
 module.exports = app;
